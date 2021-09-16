1f44ce87bf11d3e11ee2fae65bf2d8c6ed0a0fb2
Sagar Mishra <smishra@deqode.com>
2021-09-16 18:50:09 +0530

    feat(eslint): configured eslint

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+/* eslint-disable import/no-unresolved */
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import AppModule from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
